d14da1fd7627259f71502084e8e121fadfe9f2ae
Giyong8504 <kky5163@naver.com>
2023-11-14 23:08:29 +0900

    초기 설정

---
+++ src/test/java/me/Giyong8504/BlogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.Giyong8504;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
