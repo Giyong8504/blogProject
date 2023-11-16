a074ba4286ee09f60620b78c338a31bb2c5ff4f5
Giyong8504 <kky5163@naver.com>
2023-11-17 00:29:48 +0900

    블로그 글 뷰 구현

--- src/main/java/me/Giyong8504/BlogProjectApplication.java
+++ src/main/java/me/Giyong8504/BlogProjectApplication.java
@@ -2,7 +2,9 @@ package me.Giyong8504;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // created_at, update_dat 자동 업데이트
 @SpringBootApplication
 public class BlogProjectApplication {
 
--- src/main/java/me/Giyong8504/controller/BlogViewController.java
+++ src/main/java/me/Giyong8504/controller/BlogViewController.java
@@ -1,11 +1,14 @@
 package me.Giyong8504.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.Giyong8504.domian.Article;
 import me.Giyong8504.dto.ArticleListViewResponse;
+import me.Giyong8504.dto.ArticleViewResponse;
 import me.Giyong8504.service.BlogService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -23,5 +26,13 @@ public class BlogViewController {
         model.addAttribute("articles", articles); // 블로그 글 리스트 저장
 
         return "articleList"; // articleList.html 뷰 조회
+    }
+
+    @GetMapping("/articles/{id}")
+    public String getArticle(@PathVariable Long id, Model model) {
+        Article article = blogService.findById(id);
+        model.addAttribute("article", new ArticleViewResponse(article));
+
+        return "article";
     }
 }
