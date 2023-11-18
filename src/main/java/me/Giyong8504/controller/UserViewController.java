3c8b4d87f9a363650b74f04733bcf798944f736c
Giyong8504 <kky5163@naver.com>
2023-11-18 20:05:51 +0900

    로그인, 회원가입 뷰 작성

---
+++ src/main/java/me/Giyong8504/controller/UserViewController.java
@@ -0,0 +1,20 @@
+package me.Giyong8504.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class UserViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
+}
